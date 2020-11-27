858453c07d5ce7b3236d140e7cfa27aa58c44a23
rvcristiand <rvcristiand@unal.edu.co>
2020-11-27 08:47:31 -0500

    f_v function added

--- espectro_diseno.py
+++ espectro_diseno.py
@@ -3,6 +3,37 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+def f_v(tipo_suelo, a_v):
+    tipo_suelo.lower()
+
+    if tipo_suelo == 'a':
+        return 0.8
+    elif tipo_suelo == 'b':
+        return 1
+    elif tipo_suelo == 'c':
+        if a_v < 0.10:
+            return 1.7
+        else:
+            return 0.1 - (a_v - 0.1)
+    elif tipo_suelo == 'd':
+        if a_v < 0.1:
+            return 2.4
+        elif a_v < 0.2:
+            return 2.4 - 4*(a_v -0.1)
+        elif a_v < 0.4:
+            return 2 - 2*(a_v - 0.2)
+        else:
+            return 1.6 - (a_v - 0.4)
+    elif tipo_suelo == 'e':
+        if a_v < 0.1:
+            return 3.5
+        elif a_v < 0.2:
+            return 3.5 - 3*(a_v - 0.1)
+        elif a_v < 0.4:
+            return 3.2 - 4*(a_v - 0.2)
+        else:
+            return 2.4
+
 def t_0(a_v, a_a, f_v, f_a):
     return 0.1 * (a_v * f_v) / (a_a * f_a)
 
